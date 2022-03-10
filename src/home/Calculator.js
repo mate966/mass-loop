9cea0691d23677eda8460d220f2b451ef5ad5943
maTe966 <Szweda.mateusz@wp.pl>
2022-03-10 15:48:02 +0100

    create calculator function

--- src/home/Calculator.js
+++ src/home/Calculator.js
@@ -1,4 +1,38 @@
+import { useState, useEffect } from "react";
+
 const Calculator = () => {
+    const [yearlySavings, setYearlySavings] = useState(960);
+    const [costOfrecycling, setcostOfrecycling] = useState({
+        amount: 20,
+        cost: 20,
+    });
+
+    const calcYearlySavings = (a, b) => {
+        const consumption = a * b * 12;
+        const consumptionWithCrusher = 12 * (a * b - (a * b) / 5);
+        const yearlySave = consumption - consumptionWithCrusher;
+
+        return yearlySave > 10000000
+            ? null
+            : setYearlySavings(yearlySave.toFixed(2).replace(".00", ""));
+    };
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        const regex = new RegExp("^[0-9]*$");
+
+        !regex.test(value)
+            ? alert("Wpisz cyfry!")
+            : setcostOfrecycling({
+                  ...costOfrecycling,
+                  [e.target.name]: value,
+              });
+    };
+
+    useEffect(() => {
+        calcYearlySavings(costOfrecycling.amount, costOfrecycling.cost);
+    });
+
     return (
         <section id="calculator">
             <div className="calculator__content wrapper">
@@ -8,18 +42,31 @@ const Calculator = () => {
                     <p className="section-subheading">
                         Ile pojemników na szkło zapełniasz miesięcznie?
                     </p>
-                    <input type="text" value="20" className="input" />
+                    <input
+                        type="text"
+                        value={costOfrecycling.amount}
+                        name="amount"
+                        className="input"
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="calculator__input-container">
                     <p className="section-subheading">
                         Ile płacisz za wywóz jednego pojemnika na szkło?
                     </p>
-                    <input type="text" value="20" className="input" />
+                    <input
+                        type="text"
+                        value={costOfrecycling.cost}
+                        name="cost"
+                        className="input"
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="calculator__result">
                     <p className="section-subheading">
                         Dzięki kruszarce zaoszczędzisz{" "}
-                        <span className="result__span">900 zł</span> rocznie.
+                        <span className="result__span">{yearlySavings} zł</span>{" "}
+                        rocznie.
                     </p>
                 </div>
             </div>
